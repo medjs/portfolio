4196e7683d66281bcb46094106a3bd7a8a262069
Mohammed <najjarmedd@gmail.com>
2023-08-26 22:24:36 +0100

    update text effect

--- components/header.js
+++ components/header.js
@@ -2,9 +2,9 @@ import Link from 'next/link'
 
 export default function Header () {
   return (
-    <nav className='w-full'>
+    <nav className='w-full p-3'>
       <div className='sm:container mx-auto flex-wrap justify-center md:flex'>
-        <div className='sm:w-full flex justify-center items-center md:w-1/2 bg-red-100'>
+        <div className='sm:w-full flex justify-center items-center md:w-1/2'>
           <h1 className='sm:w-full text-2xl p-3 justify-center font-semibold md:ml-12 text-left'>LOGO</h1>
         </div>
         <ul className='sm:w-full font-semibold flex justify-around p-2 list-none uppercase text-black md:w-1/2'>
--- pages/projects.js
+++ pages/projects.js
@@ -6,7 +6,7 @@ export default function Projects () {
   const [isHovered, setIsHovered] = useState(false)
   return (
     <div className='container  flex-col justify-center mx-auto my-16'>
-      <div className='sm:flex flex-wrap justify-center bg-red-100'>
+      <div className='sm:flex flex-wrap justify-center'>
         {/* <div className='relative flex items-center w-96 h-96 bg-blue-100 '>
             <Image src={projet} alt='projet' className=" w-96 h-auto absolute z-0 left-0 transform transition-all hover:-translate-y-16" />
             <p className="absolute bottom-20 right-1/3 text-2xl text-white z-20 transform transition-all hover:translate-y-16">Live project</p>
@@ -18,7 +18,7 @@ export default function Projects () {
             rel='noopener noreferrer'
           >
             <div
-          className='relative flex items-center w-96 h-80 mx-auto '
+          className='relative flex items-center w-96 h-80 mx-auto bg-black'
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
@@ -32,7 +32,7 @@ export default function Projects () {
 
           
             <p className={`absolute inset-0  mt-10 font-bold flex items-center justify-center text-3xl text-green-400 z-0 transform transition-all ${
-              isHovered ? '-translate-y-10 z-20' : 'translate-y-0'
+              isHovered ? 'translate-y-24 z-20' : 'translate-y-0'
             }`} style={{ animation: 'signalEffect 1.5s infinite' }}>
 
             Go Live
@@ -40,9 +40,7 @@ export default function Projects () {
           
 
           <p
-            className={`absolute inset-0 font-semibold flex items-center justify-center text-shadow text-2xl text-white  z-20 transform transition-all ${
-              isHovered ? 'hover:text-black translate-y-32 ' : 'translate-y-0'
-            }`}
+            className={`absolute inset-0 font-semibold flex items-start justify-center text-shadow text-2xl text-white  z-20 transform transition-all`}
           >
             AirCoolPalace
           </p>
@@ -58,58 +56,8 @@ export default function Projects () {
           </p>
         </div>
       </div>
-      <div className='flex justify-center'>
-        <div className=' w-96 h-96 bg-cyan-100 text-justify p-5'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error
-            provident autem ducimus cum adipisci voluptate pariatur blanditiis
-            maiores. Explicabo aut expedita autem repudiandae fugit laudantium
-            reprehenderit, nisi cumque ut.
-          </p>
-        </div>
-        <div className='relative flex items-center w-96 h-96 bg-blue-100'>
-          <Image
-            src={projet}
-            alt='projet'
-            className='w-full h-auto absolute right-0 transform transition-all hover:-rotate-6 hover:-translate-x-16'
-          />
-        </div>
-      </div>
-      <div className='flex justify-center'>
-        <div className='relative flex items-center w-96 h-96 bg-blue-100'>
-          <Image
-            src={projet}
-            alt='projet'
-            className='w-full h-auto absolute left-0 transform transition-all hover:rotate-6 hover:translate-x-16'
-          />
-        </div>
-
-        <div className=' w-96 h-96 bg-cyan-100 text-justify p-5'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error
-            provident autem ducimus cum adipisci voluptate pariatur blanditiis
-            maiores. Explicabo aut expedita autem repudiandae fugit laudantium
-            reprehenderit, nisi cumque ut.
-          </p>
-        </div>
-      </div>
-      <div className='flex justify-center'>
-        <div className=' w-96 h-96 bg-cyan-100 text-justify p-5'>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error
-            provident autem ducimus cum adipisci voluptate pariatur blanditiis
-            maiores. Explicabo aut expedita autem repudiandae fugit laudantium
-            reprehenderit, nisi cumque ut.
-          </p>
-        </div>
-        <div className='relative flex items-center w-96 h-96 bg-blue-100'>
-          <Image
-            src={projet}
-            alt='projet'
-            className='w-full h-auto absolute right-0 transform transition-all hover:-rotate-6 hover:-translate-x-16'
-          />
-        </div>
-      </div>
+      
+      
     </div>
   )
 }
