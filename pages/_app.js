5a77f5ba857e1df26aa0dc35255b3fe0a05b662f
Mohammed <najjarmedd@gmail.com>
2023-08-28 16:12:07 +0100

    Add transition animation between routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,33 @@
 import '@/styles/globals.css'
 import Header from './components/header'
 import { usePathname } from 'next/navigation'
-export default function App({ Component, pageProps }) {
-  const pathname = usePathname()
-  
-  return(
-  <>
-  <Header pathname={pathname} />
-  <Component {...pageProps} />
-  </> 
+import { motion, AnimatePresence } from 'framer-motion'
 
+export default function App ({ Component, pageProps }) {
+  const pathname = usePathname()
+
+  return (
+    <AnimatePresence mode='wait'>
+      <motion.div key={pathname}>
+        <Header pathname={pathname} />
+        <Component {...pageProps} />
+        <motion.div
+        className='slide-in'
+        initial={{scaleY: 0}}
+        animate={{scaleY: 0}}
+        exit={{scaleY: 1}}
+        transition={{duration: 1, ease:[0.22, 1, 0.36, 1]}}
+        >
+        </motion.div>
+        <motion.div
+        className='slide-in'
+        initial={{scaleY: 1}}
+        animate={{scaleY: 0}}
+        exit={{scaleY: 0}}
+        transition={{duration: 1, ease:[0.22, 1, 0.36, 1]}}
+        >
+        </motion.div>
+      </motion.div>
+    </AnimatePresence>
   )
 }
