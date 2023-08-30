278b5973092e7826aec4521cd584d320743ac7b2
Mohammed <najjarmedd@gmail.com>
2023-08-30 23:52:36 +0100

    add gradient to home page

--- pages/components/headerHero.js
+++ pages/components/headerHero.js
@@ -1,6 +1,6 @@
 export default function HeaderHero(){
     return(
-        <section className="absolute top-0 flex flex-wrap w-full justify-between h-screen text-white bg-blue-600 items-center md:flex">
+        <section className="absolute top-0 flex flex-wrap w-full justify-between h-screen text-white bg-gradient-to-bl from-indigo-900 to-black items-center md:flex">
             <div className=" w-full flex justify-center bg-yellow-500 md:w-1/2">
                 <p>This is image placement</p>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home () {
   return (
     <main>
-      <div className='w-full h-screen bg-gray-50'>
+      <div className='w-full h-screen'>
         <HeaderHero />
         {/* <h1 className="text-3xl font-semibold transform transition-all hover:scale-200 hover:rotate-12 hover:translate-y-40 ">My Portfolio</h1> */}
       </div>
